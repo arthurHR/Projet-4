c17c8096a2ffa2cde3c44a20f668eb6d5145ed5d
arthur <richarthur@gmail.com>
2018-12-16 16:26:12 +0100

    indentation et supression du style inline

---
+++ view/frontend/homeView.php
@@ -0,0 +1,53 @@
+<?php
+ 	ob_start(); 
+    $title = "UN BILLET SIMPLE POUR L'ALASKA";
+    $subheading = "Découvrez le nouveau roman de Jean Forteroche";
+    $image = './public/img/alaska_3.jpg';
+?> 
+
+
+
+ 	<!-- Description -->
+    <article id="articleHome">
+        <div class="container">
+        	<div class="row">
+          		<div class="col-lg-8 col-md-10 mx-auto">
+	          		<h3>DESCRIPTION DU ROMAN</h3>
+		            <p>Une île sauvage du sud de l'Alaska, accessible uniquement par bateau ou par hydravion, tout en forêts humides et montagnes escarpées.
+					C'est dans ce décor que Jim décide d'emmener son fils de treize ans pour y vivre dans une cabane isolée, une année durant. 
+					Après une succession d'échecs personnels, il voit là l'occasion de prendre un nouveau départ et de renouer avec ce garçon qu'il connaît si mal.</p>
+	            	<p>Mais la rigueur de cette vie et les défaillances du père ne tardent pas à; transformer ce séjour en cauchemar, et la situation devient vite incontrôlable.</p>
+				    <p>Jusqu'au drame violent et imprévisible qui scellera leur destin</p>
+	            	<img class="img-fluid" src="public/img/livre.jpg" alt="">
+	            	<a class="btn btn-secondary" id="book" href="index.php?action=listPost">Lire le roman</a>
+            	</div>
+        	</div>
+        </div>
+    </article>
+
+    <!-- Last chapter -->
+    <div class="container" id="lastChapter">
+        <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+            	<h1>Dernier chapitre publié</h1>
+                <div class="post-preview" id="lastChapterContent">
+                    <a href="index.php?action=post&amp;id=<?= $post['id'] ?>">
+                        <h3>
+                            <?= html_entity_decode($post['title']) ?>
+                        </h3>
+                        <h4 class="post-subtitle">
+                            <?= (html_entity_decode(substr($post['content'],0 , 200))) ?>
+                        </h4>
+                    </a>
+                    <p class="post-meta">Posté par Jean Rochefort le <em> <?= $post['creation_date_fr'] ?></em></p>
+                </div>
+                <hr>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php 
+	$content = ob_get_clean();
+	require("view/template.php");
+?>
--- view/frontend/postView.php
+++ view/frontend/postView.php
@@ -1,51 +1,54 @@
-<?php
-session_start();
-
-
-ob_start(); 
-$title = htmlspecialchars($post['title']);
-$subheading =  htmlspecialchars("Posté par Jean Rochefort le ". $post['creation_date_fr']);
-$image = './public/alaska_2.jpg'; 
+<?php ob_start(); 
+    $title = strip_tags($post['title']);
+    $subheading =  strip_tags("Posté par Jean Rochefort le ". $post['creation_date_fr']);
+    $image = './public/img/alaska_2.jpg'; 
 ?>
- <div class="container">
-    <div class="row" style="margin-bottom: 50px;">
-        <div class="col-lg-12 col-md-10 mx-auto">
-                     
-            <?php if(isset($_SESSION['pseudo'])) { ?>     
-            <div class="dropdown" style="margin-top: -15%;">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                 Chapitre
-                 </button>
-                  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a role="button" class="btn btn-info" href="index.php?action=listPost" style= "text-decoration: none;" >Retour à la liste des chapitres</a>
-                   <a role="button" class="btn btn-warning" style="color : white;" href="index.php?action=updatePost&amp;id=<?= $post['id'] ?>">Modifier le chapitre</a>
-            <a role="button" class="btn btn-danger" href="index.php?action=deletPost&amp;id=<?= $post['id'] ?>" onclick="return confirm('Etes vous sûr de vouloir supprimer ce chapitre ? Attention la suppression sera definitive');">Supprimer le chapitre</a> 
-                  </div>
-</div>                 
-            
-            <?php } else { ?> 
-            <a role="button" class="btn btn-info float-right" href="index.php?action=listPost" style= "text-decoration: none;" >Retour à la liste des chapitres</a> 
-        <?php } ?>
-        </div>
-    </div>
-</div>
-<article>
+
+    <!-- navigation dans le chapitre -->
     <div class="container">
-        <div class="row">
-            <div class="col-lg-8 col-md-10 mx-auto">
-                <p><?= html_entity_decode($post['content']) ?></p> 
-            </div>
-            <div class="col-lg-10 col-md-10 mx-auto" style="margin-bottom: 50px;">
-                <a class="btn btn-primary float-left" href="index.php?action=previousPost&amp;id=<?= $post['id']?>">Chapitre précédent</a>
-                <a class="btn btn-primary float-right" href="index.php?action=nextPost&amp;id=<?= $post['id']?>">Chapitre suivant</a>
+        <div class="row" id="navigation">
+            <div class="col-lg-12 col-md-10 mx-auto">     
+                <?php 
+                if(isset($_SESSION['pseudo'])) { 
+                ?>     
+                    <div class="dropdown" id="navDropdown">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Chapitre</button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a role="button" class="btn btn-info" href="index.php?action=listPost">Retour à la liste des chapitres</a>
+                            <a role="button" class="btn btn-warning" id="updatePost" href="index.php?action=updatePost&amp;id=<?= $post['id'] ?>">Modifier le chapitre</a>
+                            <a role="button" class="btn btn-danger" id="deletePost" href="index.php?action=deletPost&amp;id=<?= $post['id'] ?>">Supprimer le chapitre</a> 
+                        </div>
+                    </div>                 
+                <?php 
+                } else { 
+                ?> 
+                    <a role="button" class="btn btn-info float-right"  id="listPost" href="index.php?action=listPost">Retour à la liste des chapitres</a> 
+                <?php 
+                } 
+                ?>
             </div>
         </div>
     </div>
-</article>
+
+    <!-- contenu du chapitre -->
+    <article>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-md-10 mx-auto">
+                    <p><?= html_entity_decode($post['content']) ?></p> 
+                </div>
+                <!-- pagination -->
+                <div class="col-lg-10 col-md-10 mx-auto" id="paginationPost">
+                    <a class="btn btn-primary float-left" href="index.php?action=previousPost&amp;id=<?= $post['id']?>">Chapitre précédent</a>
+                    <a class="btn btn-primary float-right" href="index.php?action=nextPost&amp;id=<?= $post['id']?>">Chapitre suivant</a>
+                </div>
+            </div>
+        </div>
+    </article>
 
 <?php 
-require("commentsView.php");
-$content = ob_get_clean(); 
-require("view/template.php");
+    require("commentsView.php");
+    $content = ob_get_clean(); 
+    require("view/template.php");
 ?>
 
