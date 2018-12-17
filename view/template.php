348aaad9bd28676b6cabadfde898bc0db9494fb8
arthur <richarthur@gmail.com>
2018-12-17 11:39:13 +0100

    changement de nom du bouton connexion

--- view/template.php
+++ view/template.php
@@ -28,7 +28,7 @@
         <!-- Navigation -->
         <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
             <div class="container">
-                <a class="navbar-brand" href="index.php">Jean Rochefort</a>
+                <a class="navbar-brand" href="index.php">Jean Forteroche</a>
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu<i class="fas fa-bars"></i>
                 </button>
@@ -44,7 +44,7 @@
                         if (empty($_SESSION))
                         { ?>
                             <li class="nav-item"> 
-                                <a class="nav-link" href="index.php?action=login">Admin</a>
+                                <a class="nav-link" href="index.php?action=login">Connexion</a>
                             </li>
                         <?php
                         } else { ?>
